b4b8a33abc3055aeeac6dc602b982d1d1232ec14
Sayan <Sayan.Chowdhury@bytedance.com>
2023-10-11 16:05:25 +0800

    update version

--- bytehouse_driver/__init__.py
+++ bytehouse_driver/__init__.py
@@ -28,7 +28,7 @@ from .client import Client
 from .dbapi import connect
 
 
-VERSION = (1, 0, 2)
+VERSION = (1, 0, 3)
 __version__ = '.'.join(str(x) for x in VERSION)
 
 __all__ = ['Client', 'connect']
